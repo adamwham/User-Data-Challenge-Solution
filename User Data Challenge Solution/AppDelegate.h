afee9f29bfe48901409f84b37e62bf506c3af86f
Adam Wham <AdamWham@Adams-MacBook-Pro.local>
2015-02-02 10:48:53 +0800

    Initial Commit

---
+++ User Data Challenge Solution/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  User Data Challenge Solution
+//
+//  Created by Adam Wham on 2/2/15.
+//  Copyright (c) 2015 AW. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
